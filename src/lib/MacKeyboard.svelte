bba1740ec46d03cf244d618a1ec3fc4df032a43e
Mark Jaquith <mark@jaquith.me>
2024-07-05 23:30:24 -0400

    Finish styling

--- src/lib/MacKeyboard.svelte
+++ src/lib/MacKeyboard.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-import type { KeyCodeData } from '$lib/types'
-	
-	export let prefixCls: string = 'w-mac-keyboard'
+	import type { KeyCodeData } from '$lib/types'
+
 	export let className: string = ''
 	export let style: string = ''
 	export let keyCode: number[] = []
@@ -90,37 +89,311 @@ import type { KeyCodeData } from '$lib/types'
 	]
 </script>
 
-<div class="{prefixCls} {className}" {style}>
-	<ul>
+<div class="{className} keyboard" {style}>
+	<div class="keyboard__wrap">
 		{#each keyCodeData as item}
-			<li
+			<button
 				on:mousedown={(e) => onMouseDown && onMouseDown(e, item)}
 				on:mouseup={(e) => onMouseUp && onMouseUp(e, item)}
 				class={keyCode.includes(item.keycode) ? 'pressed' : ''}
 				data-key={item.keycode}
 			>
-				{#each item.name as _item}
-					<span>{_item}</span>
+				{#each item.name as name}
+					<span>{name}</span>
 				{/each}
-			</li>
+			</button>
 		{/each}
-	</ul>
+	</div>
 </div>
 
 <style>
-	/* Include your styles here */
-	ul {
-		list-style-type: none;
+	.keyboard {
+		font-family: sans-serif;
+		height: 394px;
+		border-radius: 10px;
+		border: 1px solid #c9c9c9;
+		background: #f2f2f2;
+		box-shadow:
+			2px 0px 2px #e2e2e2 inset,
+			-2px 2px 3px #e2e2e2 inset,
+			1px 0px 0px #c1c1c1 inset,
+			0px -2px 3px #c1c1c1 inset;
+		user-select: none;
+		position: relative;
+		width: 995px;
+		margin: 0 auto;
+	}
+
+	.keyboard__wrap {
+		width: 992px;
+		margin-top: 9px;
+		padding-left: 11px;
+		position: relative;
+		float: left;
+	}
+
+	.keyboard button {
+		appearance: none;
+		background: none;
+		border: none;
+		box-shadow: none;
 		padding: 0;
-	}
-	li {
-		display: inline-block;
-		margin: 5px;
-		padding: 10px;
-		border: 1px solid #ccc;
+		margin: 0;
+		font-family: inherit;
+		font-size: inherit;
+		color: inherit;
+		text-align: inherit;
+		line-height: inherit;
 		cursor: pointer;
+		outline: none;
 	}
-	.pressed {
-		background-color: #ccc;
+
+	.keyboard button {
+		width: 62px;
+		height: 62px;
+		float: left;
+		list-style: none;
+		margin-right: 5px;
+		margin-bottom: 5px;
+		background: #151515;
+		color: #c8c8c8;
+		text-align: center;
+		line-height: 62px;
+		font-size: 12px;
+		border-radius: 8px;
+		border: 1px solid #3a3a3a;
+		box-shadow:
+			1px 0px 0px #000000,
+			0px 1px 0px #000000,
+			-1px 0px 0px #000000,
+			0px -1px 0px #000000;
+		box-sizing: initial;
+		transition: all 0.4s ease-in;
+		user-select: none;
+		cursor: pointer;
+		position: relative;
+	}
+
+	.keyboard button:active,
+	.keyboard button.pressed {
+		color: #52f800;
+		background-color: #100068;
+		border: 1px solid #332376;
+		transition: 1ms linear;
+	}
+
+	.keyboard button:nth-child(1) {
+		width: 99px;
+		height: 28px;
+		line-height: 28px;
+		text-indent: 1em;
+		text-align: left;
+	}
+
+	.keyboard button:nth-child(2),
+	.keyboard button:nth-child(3),
+	.keyboard button:nth-child(4),
+	.keyboard button:nth-child(5),
+	.keyboard button:nth-child(6),
+	.keyboard button:nth-child(7),
+	.keyboard button:nth-child(8),
+	.keyboard button:nth-child(9),
+	.keyboard button:nth-child(10),
+	.keyboard button:nth-child(11),
+	.keyboard button:nth-child(12),
+	.keyboard button:nth-child(13),
+	.keyboard button:nth-child(14) {
+		width: 60px;
+		height: 28px;
+		line-height: 28px;
+	}
+
+	.keyboard button:nth-child(15),
+	.keyboard button:nth-child(16),
+	.keyboard button:nth-child(17),
+	.keyboard button:nth-child(18),
+	.keyboard button:nth-child(19),
+	.keyboard button:nth-child(20),
+	.keyboard button:nth-child(21),
+	.keyboard button:nth-child(22),
+	.keyboard button:nth-child(23),
+	.keyboard button:nth-child(24),
+	.keyboard button:nth-child(25),
+	.keyboard button:nth-child(26),
+	.keyboard button:nth-child(27),
+	.keyboard button:nth-child(40),
+	.keyboard button:nth-child(41),
+	.keyboard button:nth-child(42),
+	.keyboard button:nth-child(53),
+	.keyboard button:nth-child(54),
+	.keyboard button:nth-child(64),
+	.keyboard button:nth-child(65),
+	.keyboard button:nth-child(66) {
+		padding: 7px 0;
+		height: 48px;
+	}
+
+	.keyboard button:nth-child(15) span,
+	.keyboard button:nth-child(16) span,
+	.keyboard button:nth-child(17) span,
+	.keyboard button:nth-child(18) span,
+	.keyboard button:nth-child(19) span,
+	.keyboard button:nth-child(20) span,
+	.keyboard button:nth-child(21) span,
+	.keyboard button:nth-child(22) span,
+	.keyboard button:nth-child(23) span,
+	.keyboard button:nth-child(24) span,
+	.keyboard button:nth-child(25) span,
+	.keyboard button:nth-child(26) span,
+	.keyboard button:nth-child(27) span,
+	.keyboard button:nth-child(40) span,
+	.keyboard button:nth-child(41) span,
+	.keyboard button:nth-child(42) span,
+	.keyboard button:nth-child(53) span,
+	.keyboard button:nth-child(54) span,
+	.keyboard button:nth-child(64) span,
+	.keyboard button:nth-child(65) span,
+	.keyboard button:nth-child(66) span {
+		line-height: 23px;
+		height: 23px;
+		width: 100%;
+		float: left;
+		font-size: 14px;
+	}
+
+	.keyboard button:nth-child(28) span,
+	.keyboard button:nth-child(55) span,
+	.keyboard button:nth-child(67) span {
+		line-height: 14px;
+		margin-top: 43px;
+		float: right;
+		margin-right: 8px;
+	}
+
+	.keyboard button:nth-child(30),
+	.keyboard button:nth-child(31),
+	.keyboard button:nth-child(32),
+	.keyboard button:nth-child(33),
+	.keyboard button:nth-child(34),
+	.keyboard button:nth-child(35),
+	.keyboard button:nth-child(36),
+	.keyboard button:nth-child(37),
+	.keyboard button:nth-child(38),
+	.keyboard button:nth-child(39),
+	.keyboard button:nth-child(44),
+	.keyboard button:nth-child(45),
+	.keyboard button:nth-child(46),
+	.keyboard button:nth-child(47),
+	.keyboard button:nth-child(48),
+	.keyboard button:nth-child(49),
+	.keyboard button:nth-child(50),
+	.keyboard button:nth-child(51),
+	.keyboard button:nth-child(52),
+	.keyboard button:nth-child(57),
+	.keyboard button:nth-child(58),
+	.keyboard button:nth-child(59),
+	.keyboard button:nth-child(60),
+	.keyboard button:nth-child(61),
+	.keyboard button:nth-child(62),
+	.keyboard button:nth-child(63) {
+		font-size: 18px;
+		line-height: 64px;
+	}
+
+	.keyboard button:nth-child(29) span,
+	.keyboard button:nth-child(43) span,
+	.keyboard button:nth-child(56) span,
+	.keyboard button:nth-child(68) span,
+	.keyboard button:nth-child(69) span,
+	.keyboard button:nth-child(70) span,
+	.keyboard button:nth-child(71) span,
+	.keyboard button:nth-child(73) span,
+	.keyboard button:nth-child(74) span {
+		line-height: 14px;
+		margin-top: 43px;
+		text-indent: 5px;
+		float: left;
+	}
+
+	.keyboard button:nth-child(28),
+	.keyboard button:nth-child(29) {
+		width: 73px;
+	}
+
+	.keyboard button:nth-child(43),
+	.keyboard button:nth-child(55) {
+		width: 102px;
+	}
+
+	.keyboard button:nth-child(43):active span:first-child {
+		background: #52f800;
+	}
+
+	.keyboard button:nth-child(43) span:first-child {
+		position: absolute;
+		top: -36px;
+		left: 5px;
+		width: 6px;
+		height: 6px;
+		background: #fff;
+		border-radius: 3px;
+	}
+
+	.keyboard button:nth-child(55) span:first-child,
+	.keyboard button:nth-child(70) span:first-child {
+		position: absolute;
+		right: 0px;
+		top: -37px;
+		font-size: 12px;
+		line-height: 12px;
+	}
+
+	.keyboard button:nth-child(70) span:first-child {
+		left: 0px;
+		text-align: left;
+	}
+
+	.keyboard button:nth-child(74) span:first-child {
+		position: absolute;
+		left: 0;
+		top: -37px;
+		font-size: 12px;
+		line-height: 12px;
+	}
+
+	.keyboard button:nth-child(56) {
+		width: 136px;
+	}
+
+	.keyboard button:nth-child(67) {
+		width: 137px;
+	}
+
+	.keyboard button:nth-child(71),
+	.keyboard button:nth-child(73) {
+		width: 70px;
+	}
+
+	.keyboard button:nth-child(72) {
+		width: 333px;
+	}
+
+	.keyboard button:nth-child(76),
+	.keyboard button:nth-child(78) {
+		height: 29px;
+		line-height: 29px;
+	}
+
+	.keyboard button:nth-child(76) {
+		border-bottom-left-radius: 0;
+		border-bottom-right-radius: 0;
+	}
+
+	.keyboard button:nth-child(78) {
+		border-top-left-radius: 0;
+		border-top-right-radius: 0;
+		position: absolute;
+		bottom: 0;
+		right: 84px;
 	}
 </style>
